f7a69032f9d30ea752c938e8f035b30de5d0359b
Kamau-LCC <129826414+Kamau-LCC@users.noreply.github.com>
2023-10-08 22:26:33 -0700

    Delete src directory

--- src/components/Board.js
+++
@@ -1,9 +0,0 @@
-function Board ({className,i,eventHandler}){
-    return (
-        <div className={className}
-        id={i} 
-        onClick={eventHandler}>
-        </div> 
-    );
-}
-export default Board;
--- src/components/Status.js
+++
@@ -1,9 +0,0 @@
-const Status=({status,value})=>{
-    console.log('Status Component');
-    return ( 
-        <div className="pb-2" id='status'>{status}{value}
-        
-        </div>
-    );
-}
-export default Status;
